986077992ff9e9ad2290cbeb450b1ce93386dca3
Ruben Pla <ruben.pla@alten.es>
2017-10-17 17:13:56 +0200

    [APPLICATION] Application class created

--- app/src/main/java/com/rubenpla/develop/techtestapp/application/TechTestApplication.java
+++ app/src/main/java/com/rubenpla/develop/techtestapp/application/TechTestApplication.java
@@ -3,25 +3,38 @@ package com.rubenpla.develop.techtestapp.application;
 import android.app.Application;
 import android.content.Context;
 
-import com.rubenpla.develop.techtestapp.util.preferences.SharedPreferencesAgent;
+import com.rubenpla.develop.techtestapp.db.DataStore;
+import com.rubenpla.develop.techtestapp.preferences.SharedPreferencesAgent;
 
 public class TechTestApplication extends Application {
 
     private final String TAG = TechTestApplication.class.getSimpleName();
 
-    private SharedPreferencesAgent preferencesAgent;
+    private static SharedPreferencesAgent preferencesAgent;
+    private static DataStore dataStoreDB;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        initSharedPreferencesController(this);
+        initSharedPreferencesController(this.getApplicationContext());
+        initDataStoreDB(this.getApplicationContext());
     }
 
     private void initSharedPreferencesController(Context context) {
         preferencesAgent = new SharedPreferencesAgent(context);
+    }
 
+    private void initDataStoreDB(Context context) {
+        dataStoreDB = new DataStore(context, DataStore.APP_DATABASE_NAME);
+    }
 
+    public static SharedPreferencesAgent getSharedPreferencesAgent() {
+        return preferencesAgent;
+    }
+
+    public static DataStore getDataStoreDB() {
+        return dataStoreDB;
     }
 }
 
